4d6a1928337d29e879f11a7fc8ac8c7076001945
Arsenij <79141113+PandaGrom@users.noreply.github.com>
2021-03-09 20:16:35 +0300

    init: add code

--- src/dream-team.js
+++ src/dream-team.js
@@ -3,4 +3,15 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function createDreamTeam(/* members */) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  if (!isNaN(members) || !members || members.length === 0 || members.__proto__ === Object.prototype) {
+    return false;
+  }
+  const newName = [];
+  for (let member of members) {
+    if (typeof member !== 'string') {
+      member = '';
+    }
+     newName.push(member.trim().substr(0,1).toUpperCase());
+  }
+  return newName.sort().join('');
 };
