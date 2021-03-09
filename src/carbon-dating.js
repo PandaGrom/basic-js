c3e90d37899b4f205cccb1530f27d5cc31422528
Arsenij <79141113+PandaGrom@users.noreply.github.com>
2021-03-09 20:04:48 +0300

    init: add code

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,4 +6,15 @@ const HALF_LIFE_PERIOD= 5730;
 module.exports = function dateSample(/* sampleActivity */) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
-};
+  
+    if (typeof sampleActivity !== 'string' || !sampleActivity || isNaN(sampleActivity)) {
+      return false;
+    } else {
+      sampleActivity = +sampleActivity;
+      if (sampleActivity == 0 || sampleActivity > 15 || sampleActivity < 0) {
+        return false;
+      }
+      return Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivity) * HALF_LIFE_PERIOD) / 0.693);
+    }
+  };
+
