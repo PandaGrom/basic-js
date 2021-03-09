21544339f4fac6f56ced664079375e79e8ed336c
Arsenij <79141113+PandaGrom@users.noreply.github.com>
2021-03-09 20:19:23 +0300

    init: add code

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
+module.exports = function createDreamTeam(members) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
   if (!isNaN(members) || !members || members.length === 0 || members.__proto__ === Object.prototype) {
